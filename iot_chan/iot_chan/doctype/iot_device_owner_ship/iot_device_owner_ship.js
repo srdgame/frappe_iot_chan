28c593dc20339b3739c75f65dd1c6c22987c105a
Dirk Chang <dirk@kooiot.com>
2020-08-12 17:35:28 +0800

    Import IOT Device from upper center

---
+++ iot_chan/iot_chan/doctype/iot_device_owner_ship/iot_device_owner_ship.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, dirk and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('IOT Device Owner Ship', {
+	// refresh: function(frm) {
+
+	// }
+});

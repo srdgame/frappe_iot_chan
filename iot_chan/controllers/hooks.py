136b188a985edabc0f449045b1c7f0610226e7ef
Dirk Chang <dirk@kooiot.com>
2020-08-13 15:31:22 +0800

    do not blocks during data import

--- iot_chan/controllers/hooks.py
+++ iot_chan/controllers/hooks.py
@@ -10,6 +10,8 @@ from iot_chan.iot_chan.doctype.iot_chan_settings.iot_chan_settings import IOTCha
 
 def validate_iot_device(doc, method):
 	if IOTChanSettings.get_enable_upper_center() == 1:
+		if frappe.flags.in_import:
+			return
 		if frappe.get_value("IOT Virtual Device", doc.sn, "name"):
 			return
 
@@ -18,24 +20,34 @@ def validate_iot_device(doc, method):
 
 def validate_app_category(doc, method):
 	if IOTChanSettings.get_enable_upper_center() == 1:
+		if frappe.flags.in_import:
+			return
 		throw(_("Child IOT Center blocking check"))
 
 
 def validate_iot_hardware_architecture(doc, method):
 	if IOTChanSettings.get_enable_upper_center() == 1:
+		if frappe.flags.in_import:
+			return
 		throw(_("Child IOT Center blocking check"))
 
 
 def validate_app_developer(doc, method):
 	if IOTChanSettings.get_enable_upper_center() == 1:
+		if frappe.flags.in_import:
+			return
 		throw(_("Child IOT Center blocking check"))
 
 
 def validate_iot_application(doc, method):
 	if IOTChanSettings.get_enable_upper_center() == 1:
+		if frappe.flags.in_import:
+			return
 		throw(_("Child IOT Center blocking check"))
 
 
 def validate_iot_application_version(doc, method):
 	if IOTChanSettings.get_enable_upper_center() == 1:
+		if frappe.flags.in_import:
+			return
 		throw(_("Child IOT Center blocking check"))

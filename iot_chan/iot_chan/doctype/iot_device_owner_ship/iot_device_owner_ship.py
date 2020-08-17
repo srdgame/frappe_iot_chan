73f87810bf47a7c82d31729bad232b9a52ed6a7f
Dirk Chang <dirk@kooiot.com>
2020-08-17 16:18:53 +0800

    added device name in owner ship doctype

--- iot_chan/iot_chan/doctype/iot_device_owner_ship/iot_device_owner_ship.py
+++ iot_chan/iot_chan/doctype/iot_device_owner_ship/iot_device_owner_ship.py
@@ -3,8 +3,12 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+
 class IOTDeviceOwnerShip(Document):
-	pass
+	def validate(self):
+		self.device_name = frappe.get_value("IOT Device", self.device, "dev_name")
+		if self.comments is None:
+			self.comments = frappe.get_value("IOT Device", self.device, "description")
